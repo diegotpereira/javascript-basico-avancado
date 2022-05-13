390ad8902e97dc484812ee42771dc5e263617bac
diegotpereira <diegoteixeirapereira@hotmail.com>
2022-05-13 15:53:54 -0300

    criação do app com exemplo template strings

---
+++ exemplos-praticos/template-strings/js/comTemplateStrings.js
@@ -0,0 +1,19 @@
+var produto = prompt('Nome do produto')
+var valor = prompt('Preço')
+
+var lista = compra(produto, valor)
+
+function compra(produto, valor) {
+    return `<table border="1" style='width:70%; text-align:center'
+              <tr>
+                 <th>Produto</th>
+                 <th>Valor da Compra</th>
+              </tr>
+              <tr>
+                 <td>${produto}</td>
+                 <td>${valor}</td>
+              </tr>
+           </table>`;
+}
+
+document.getElementById('tabelaCompras').innerHTML = lista
