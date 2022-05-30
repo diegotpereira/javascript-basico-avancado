35e9e53367005d6e1eabadaebd6aad46f9f9c3b6
Diegotpereira <diegoteixeirapereira@hotmail.com>
2022-05-30 17:39:34 -0300

    criação parcial do app com exemplo de cenas animadas.

--- avancado/cenas-animadas/balloon-popper2/docs/js/app.js
+++ avancado/cenas-animadas/balloon-popper2/docs/js/app.js
@@ -7,7 +7,7 @@ let windowWidth = window.innerWidth
 let windowHeight = window.innerHeight
 let body = document.body 
 
-let pontuacao = document.querySelector('.score')
+let pontuacao = document.querySelectorAll('.score')
 let totalPontos = document.querySelectorAll('.points')
 let num = 0
 let total = 100
@@ -30,7 +30,7 @@ function criarBalao() {
     let aleatorio = Math.floor(Math.random() * cores.length)
     div.className = 'balloon balloon-' + cores[aleatorio]
 
-    aleatorio = Math.floor(Math.random() + (windowWidth - 100))
+    aleatorio = Math.floor(Math.random() * (windowWidth - 100))
     div.style.left = aleatorio + 'px'
     div.dataset.number = atualBalao
     atualBalao++
@@ -38,6 +38,7 @@ function criarBalao() {
 
     switch (balaoContar) {
         case 10:
+			balaoVelocidade += .5
             break;
         case 20:
 			balaoVelocidade += .5;
@@ -87,15 +88,240 @@ function animarBalao(elem, velocidade) {
         }
     }
 }
-function iniciarJogo() {
-        if (!fimJogo && num !== total) {
-            criarBalao()
-        }
+
+function criarPlano() {
+	if(!planoExiste) {
+		// cria div container objeto do planoExiste
+		let div = document.createElement('div')
+		div.className = 'plano'
+		div.style.top = BuscarNumeroAleatorio(0, 250) + 'px'
+		div.style.left = '1200px'
+		div.style.width = 'fit-content'
+		div.style.height ='fit-content'
+		div.style.position = 'absolute'
+		planoExiste = true
+
+		// criar plano de imagem e definir estilo
+		let plano = document.createElement('img')
+		plano.src = 'imagens/plane-pulling-banner.png'
+		plano.style.float = 'right'
+		plano.style.height = '175px'
+		plano.style.left = '-1200px'
+		plano.style.position = 'absolute'
+
+		// criar logo imagem e definir estilo
+		let logo = document.createElement('img')
+		selecionarLogo = logos[BuscarNumeroAleatorio(0, (logos.length - 1))]
+		logo.src = selecionarLogo
+		logo.style.position = 'absolute'
+		logo.style.left = '-1133px'
+		logo.style.top = '8px'
+		logo.style.width = '165px'
+		logo.style.height = 'auto'
+
+			//Insert images into container div	
+		div.appendChild(plano);
+		div.appendChild(logo);
+		//insert div into body
+		body.appendChild(div);
+		animarPlano(div)
+	}
+}
+function animarPlano(elem) {
+	let posicao = -400
+	let intervalo = setInterval(frame, 10)
+	let taxaVelocidade = 2
+
+	function frame() {
+		if(posicao >= (windowWidth + 1200) && !fimJogo) {
+			clearInterval(intervalo)
+			planoExiste = false
+			criarPlano()
+		} else if(fimJogo) {
+			clearInterval(intervalo)
+			planoExiste = false
+		} else {
+			posicao += taxaVelocidade
+			elem.style.left = posicao + 'px'
+		}
+	}
+}
+function BuscarNumeroAleatorio(min, max) {
+	min = Math.ceil(min)
+	max = Math.floor(max)
+
+	return Math.floor(Math.random() * (max - min + 1 )) + min
+}
+function deletarBalao(elem) {
+	elem.remove()
+	num++
+
+	switch(elem.className) {
+		case "balloon balloon-violet":
+		pontos +=1;
+		break;
+		case "balloon balloon-orange":
+		pontos +=1;
+		break;
+		case "balloon balloon-green":
+		pontos +=1;
+		break;
+		case "balloon balloon-red":
+		pontos +=1;
+		break;
+		case "balloon balloon-yellow":
+		pontos +=1;
+		break;
+		case "balloon balloon-blue":
+		pontos +=1;
+		break;
+		case "balloon balloon-black":
+		if (pontos > 5) {
+			pontos -= 5;
+		} else {
+			pontos = 0;
+		}
+		break;
+	}
+	atualizarPontuacao()
+}
+function deletarPlano(elem) {
+	elem.remove()
+}
+function atualizarPontuacao() {
+	for(let index = 0; index < pontuacao.length; index++) {
+		pontuacao[index].textContent = num
+	}
+	for(index = 0; index < totalPontos.length; index++) {
+		totalPontos[index].textContent = pontos
+	}
 }
 
+function atualizarEstrelas() {
+	if (pontos < 70) {
+		estrelas = 'imagens/1-star.png'
+	} else if (pontos <= 80) {
+		estrelas = 'imagens/2-star.png'
+	} else if (pontos <= 90) {
+		estrelas = 'imagens/3-star.png'
+	} else if (pontos <= 100) {
+		estrelas = 'imagens/4-star.png'
+	} else {
+		estrelas = 'imagens/5-star.png'
+	}
+	var image = document.createElement('img')
+	image.src = estrelas
+	image.style.height = '100px'
+	image.id = 'estrela'
+	document.getElementById('win-starsEarned').appendChild(image)
+	document.getElementById("lose-starsEarned").appendChild(image)
+}
+function removerEstrelas() {
+	var removerVencedorEstrelas = document.getElementById('star')
+	var removerPerdeuEstrelas = document.getElementById('star')
+
+	removerVencedorEstrelas.remove()
+	removerPerdeuEstrelas.remove()
+}
+function iniciarJogo() {
+	reiniciarJogo()
+	document.querySelector('.score-block').style.display = 'block'
+    criarPlano()
+	let timeout = 0
+	let loop = setInterval(function() {
+		timeout = Math.floor(Math.random() * 600 - 100)
+		if (!fimJogo && num !== total) {
+            criarBalao()
+        } else if(num !== total) {
+			clearInterval(loop)
+			totalShadow.style.display = 'flex'
+			document.querySelector('.score-block').style.display = 'none'
+			totalShadow.querySelector('.lose').style.display = 'block'
+			atualizarEstrelas()
+		} else {
+			clearInterval(loop)
+			totalShadow.style.display = 'flex'
+			document.querySelector('.score-block').style.display = 'none'
+			totalShadow.querySelector('.win').style.display = 'block'
+			atualizarEstrelas()
+		}
+	}, 800 + timeout)
+}
+function reiniciarJogo() {
+	let paraRemover = document.querySelectorAll('.balloon')
+	for(let i = 0; i < paraRemover.length; i++) {
+		paraRemover[i].remove()
+	}
+	let paraRemoverPlanos = document.querySelectorAll('.plano')
+	for(let i = 0; i < paraRemoverPlanos.length; i++) {
+		paraRemoverPlanos[i].remove()
+	}
+	fimJogo = false
+	num = 0
+	pontos = 0
+	balaoVelocidade = 1
+	balaoContar = 0
+	atualizarPontuacao()
+}
+window.addEventListener('resize', function(event) {
+	windowWidth = window.innerWidth
+	windowHeight = window.innerHeight
+})
+document.addEventListener('click', function(event) {
+	if (event.target.classList.contains('balloon')) {
+		deletarBalao(event.target)
+	}
+})
+document.querySelector('.restart').addEventListener('click', function() {
+	totalShadow.style.display = 'none'
+	totalShadow.querySelector('.win').style.display = 'none'
+	totalShadow.querySelector('.lose').style.display = 'none'
+	removerEstrelas()
+	iniciarJogo()
+})
+document.querySelector('#win-back').addEventListener('click', function() {
+	window.location.href = 'http://www.flywithbutchohare.com'
+})
+document.querySelector('#lost-back').addEventListener('click', function() {
+	window.location.href = 'http://www.flywithbutchohare.com'
+})
+document.querySelector('#win-start').addEventListener('click', function() {
+	totalShadow.style.display = 'none'
+	totalShadow.querySelector('.win').style.display = 'none'
+	totalShadow.querySelector('.lose').style.display = 'none'
+	removerEstrelas()
+	iniciarJogo()
+})
+document.querySelector('#lost-start').addEventListener('click', function() {
+	totalShadow.style.display = 'none'
+	totalShadow.querySelector('.win').style.display = 'none'
+	totalShadow.querySelector('.lose').style.display = 'none'
+	removerEstrelas()
+	iniciarJogo()
+})
 iniciarBtn.addEventListener('click', function() {
     iniciarJogo()
     document.querySelector('.start-game-window').style.display = 'none'
     document.querySelector('.score-block').style.display = 'block'
     console.log("iniciar jogo");
 })
+voltarBtn.addEventListener('click', function() {
+	window.location.href = 'http://www.flywithbutchohare.com';
+});
+// Função de tempo limite para retornar ao Game Center após período de inatividade.
+// (() => {
+// 	var t 
+// 	window.onmousemove = redefinirTempo; // captura os movimentos do mouse
+//     window.onmousedown = redefinirTempo; // captura os movimentos do mouse
+//     window.onclick = redefinirTempo;     // capta cliques do mouse
+//     window.onscroll = redefinirTempo;    // pega rolando
+//     window.onkeypress = redefinirTempo;  //captura as ações do teclado
+
+// 	function recarregar() {
+// 		window.location.href = 'http://www.flywithbutchohare.com';  
+// 	}
+// 	function redefinirTempo() {
+// 		clearInterval(t)
+// 		t = setTimeout(recarregar, 120000)
+// 	}
+// })()
