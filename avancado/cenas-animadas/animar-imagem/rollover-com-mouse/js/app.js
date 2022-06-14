b28b113835f1e30409ab8db1e41f4b91f0b93983
diegotpereira <diegoteixeirapereira@hotmail.com>
2022-06-14 08:28:11 -0300

    criação do app com exemplo de animação com mouse.

---
+++ avancado/cenas-animadas/animar-imagem/rollover-com-mouse/js/app.js
@@ -0,0 +1,8 @@
+if (document.images) {
+    
+    var image1 = new Image()
+    image1.src = "../rollover-com-mouse/img/html.gif"
+
+    var image2 = new Image()
+    image2.src = "../rollover-com-mouse/img/http.gif"
+}
