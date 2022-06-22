7c8d9308c73cb2737b57d3f234047efb760e4294
diegotpereira <diegoteixeirapereira@hotmail.com>
2022-06-22 10:42:17 -0300

    criação do app com exemplo de preloader.

---
+++ avancado/cenas-animadas/animacao-pre-carregador/js/app.js
@@ -0,0 +1,11 @@
+var preCarregador = document.createElement('div')
+
+preCarregador.className = 'preloader'
+
+preCarregador.innerHTML = '<div class="b-ico-preloader"></div><div class="spinner"></div>';
+
+document.body.appendChild(preCarregador)
+
+window.addEventListener('load', function() {
+
+})
