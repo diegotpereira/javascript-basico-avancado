88fda7996c28bd6046a1bac274d6a573970d800f
diegotpereira <diegoteixeirapereira@hotmail.com>
2022-06-23 11:02:26 -0300

    ãdiconado eventos de mouse.

--- avancado/cenas-animadas/breaking-bad-walter-white/js/app.js
+++ avancado/cenas-animadas/breaking-bad-walter-white/js/app.js
@@ -1,12 +1,24 @@
 var camera, cena, renderizador
 var mundo, walter
-var semiLuz, dirLuz, fundoLuz
+var semiLuz, dirLuz, fundoLuz, ehAcima
 
 var container = {
     width: 0,
     height: 0
 }
 
+var mouse = {
+    x: {
+        atual: 0,
+        anterior: 0,
+        calc: 0
+    },
+    y: {
+        atual: 0,
+        anterior: 0,
+        calc: 0
+    }
+}
 function init() {
     container.width = window.innerWidth
     container.height = window.innerHeight
@@ -30,22 +42,63 @@ function init() {
     mundo.appendChild(renderizador.domElement)
 
     window.addEventListener('load', function() {
-        // document.addEventListener('mousemove', mousemove, false)
-        // window.addEventListener('resize', onWindowResize, false)
-        // document.addEventListener('mouseup', mouseup, false)
-        // documment.addEventListener('mousedown', mousedown, false)
-        // document.addEventListener('touchend', touchend, false)
-        // document.addEventListener('touchmove', touchmove, false)
+        document.addEventListener('mousemove', mousemove, false)
+        window.addEventListener('resize', onWindowResize, false)
+        document.addEventListener('mouseup', mouseup, false)
+        document.addEventListener('mousedown', mousedown, false)
+        document.addEventListener('touchend', touchend, false)
+        document.addEventListener('touchmove', touchmove, false)
     })
 }
 
-function mousemove() {}
+function onWindowResize() {
+    container.width = window.innerWidth
+    container.height = window.innerHeight
+    camera.aspect = container.width / container.height
+    camera.updateProjectionMatrix()
+    renderizador.setSize(window.innerWidth, window.innerHeight)
+}
+function mousemove(e) {
+    mouse.x.atual = e.clientX;
+    mouse.y.atual = e.clientY
+    mouse.x.calc = mouse.x.atual - (container.width / 2)
+    mouse.y.calc = mouse.y.atual - (container.height / 2)
+}
+
+function touchend(e) {
+    if (ehAcima) {
+        ehAcima = false
+    } else {
+        mousedown(e)
+    }
+}
+
+function touchmove(e) {
+    if (e.touches.length === 1) {
+        e.preventDefault()
+
+        mouse.x.atual = e.touches[0].pageX,
+        mouse.y.atual = e.touches[0].pageY
+        mouse.x.calc = mouse.x.atual - (container.width / 2)
+        mouse.y.calc = mouse.y.atual - (container.height / 2)
+    }
+}
+
+function mouseup(e) {
+    ehAcima = false
+    console.log(ehAcima);
+}
+
+function mousedown(e) {
+    ehAcima = true 
+    console.log(ehAcima);
+}
 
 function addLuzes() {
     semiLuz = new THREE.HemisphereLight(0xffffff, 0xffffff, 0.6)
 
     dirLuz = new THREE.DirectionalLight(0xffffff, .8)
-    dirLuz.position.set(200, 200, 2000)
+    dirLuz.position.set(200, 200, 200)
     dirLuz.costumizarSombra = true 
     dirLuz.sombraEscuridao = .1
 
@@ -69,11 +122,12 @@ function Walter() {
     this.informalSmokingMat = "#ffc107"
     this.informalLegsMMat = "#755b0b"
     this.informalZipperMat = "#755b0b"
+    this.informalSapatoMat = "#907637"
 
     this.formalSmokingMat = '#333'
     this.formalLegsMMat = '#222'
-    this.formalSapatoMat = '#585858'
     this.formalZipperMat = "white";
+    this.formalSapatoMat = '#585858'
 
     this.hatMat = new THREE.MeshLambertMaterial({
         color: "#333",
@@ -165,22 +219,22 @@ function Walter() {
     var barba = new THREE.BoxGeometry(140, 130, 10)
     this.barba = new THREE.Mesh(barba, this.barbaMat)
     this.barba.position.x = 0
-    this.barba.position.y = 160
-    this.barba.positionz = -140
+    this.barba.position.y = -140
+    this.barba.positionz = 160
 
     // mout
     var mout = new THREE.BoxGeometry(90, 60, 50)
     this.mout = new THREE.Mesh(mout, this.skinMat)
     this.mout.position.x = 0
-    this.mout.position.y = 155
-    this.mout.position.z = -130
+    this.mout.position.y = -130
+    this.mout.position.z = 155
 
     // labios
     var labio = new THREE.BoxGeometry(40, 20, 50)
     this.labio = new THREE.Mesh(labio, this.labioMat)
     this.labio.position.x = 0
-    this.labio.position.y = 162
-    this.labio.position.z = -120
+    this.labio.position.y = -120
+    this.labio.position.z = 162
 
     // chapeu
     var chapeu = new THREE.BoxGeometry(320, 120, 290)
@@ -192,8 +246,8 @@ function Walter() {
     var chapeuBotao = new THREE.BoxGeometry(400, 40, 380)
     this.chapeuBotao = new THREE.Mesh(chapeuBotao, this.hatMat)
     this.chapeuBotao.position.x = 0
-    this.chapeuBotao.position.y = 0 
-    this.chapeuBotao.position.z = 100   
+    this.chapeuBotao.position.y = 100
+    this.chapeuBotao.position.z = 0   
 
     // corpo
     var corpo = new THREE.BoxGeometry(300, 250, 600)
@@ -238,11 +292,11 @@ function Walter() {
     this.zipper.position.z = 300
 
     // pernas
-    var pernas = new THREE.BoxGeometry(10, 130, 5)
+    var pernas = new THREE.BoxGeometry(200, 300, 300)
     this.pernas = new THREE.Mesh(pernas, this.smokingMat)
-    this.pernas.position = 0
-    this.pernas.position = -200
-    this.pernas.position = 100
+    this.pernas.position.x = 0
+    this.pernas.position.y = -200
+    this.pernas.position.z = 100
 
     // coxa
     var coxas = new THREE.BoxGeometry(10, 130, 5)
@@ -283,11 +337,86 @@ function Walter() {
 
     this.threegroup.add(this.cabeca)
     this.threegroup.add(this.corpo)
+
+    this.atualizar = function() {
+        //mover corpo
+        this.bodyRY = calc(mouse.x.calc, -400, 400, -Math.PI / 20, Math.PI / 20);
+        this.bodyRX = calc(mouse.y.calc, -400, 400, -Math.PI / 90, Math.PI / 90);
+        //mover cabeça
+        this.headRY = calc(mouse.x.calc, -200, 200, -Math.PI / 4, Math.PI / 4);
+        this.headRX = calc(mouse.y.calc, -200, 200, -Math.PI / 4, Math.PI / 4);
+
+        this.rotate(10);
+    }
+    this.rotate = function(velocidade) {
+        if (ehAcima) {
+            if (this.barba.scale.y < 2) {
+                this.barba.scale.y += 0.02
+                this.barba.position.y -= 1.3
+                this.corpo.position.z -= 2
+            }
+            mundo.classList.add('noBreathe')
+            this.corpo.material.color = new THREE.Color(this.informalSmokingMat)
+            this.coxas.material.color = new THREE.Color(this.informalLegsMMat)
+            this.zipper.material.color = new THREE.Color(this.informalZipperMat)
+            this.sapatos.material.color = new THREE.Color(this.informalSapatoMat)
+
+            this.zipper.scale.x = this.zipper.scale.x = 0.2
+            this.chapeu.scale.x = this.chapeuBotao.scale.x = 0
+            this.chapeu.scale.y = this.chapeuBotao.scale.y = 0
+            this.chapeu.scale.z = this.chapeuBotao.scale.z = 0
+            this.labio.scale.x = 0.5
+
+            
+            this.retinaEsquerda.rotateZ(0.1)
+            this.retinaDireita.rotateZ(-0.1)
+            this.maoEsquerda.rotateY(0.1)
+            this.maoDireita.rotateY(-0.1)
+
+        } else {
+            mundo.classList.remove('noBreathe')
+
+            this.barba.position.set(0, -140, 160)
+            this.barba.scale.y = 1
+            this.corpo.position.z = 100
+
+            this.corpo.material.color = new THREE.Color(this.formalSmokingMat)
+            this.coxas.material.color = new THREE.Color(this.formalLegsMMat)
+            this.zipper.material.color = new THREE.Color(this.formalZipperMat)
+            this.sapatos.material.color = new THREE.Color(this.formalSapatoMat)
+
+            this.zipper.scale.x = this.zipper.scale.x =1
+            this.chapeu.scale.x = this.chapeuBotao.scale.x = 1
+            this.chapeu.scale.y = this.chapeuBotao.scale.y = 1
+            this.chapeu.scale.z = this.chapeuBotao.scale.z = 1
+            this.labio.scale.x = 1
+
+            this.retinaEsquerda.rotation.z = 0
+            this.retinaDireita.rotation.z = 0
+            this.maoEsquerda.rotation.y = 0
+            this.maoDireita.rotation.y = 0
+        }
+        this.corpo.rotation.y += (this.bodyRY - this.corpo.rotation.y) / velocidade;
+        this.corpo.rotation.x += (this.bodyRX - this.corpo.rotation.x) / velocidade;
+        this.cabeca.scale.x = this.cabeca.scale.y = this.cabeca.scale.z = 1;
+        this.cabeca.rotation.y += (this.headRY - this.cabeca.rotation.y) / velocidade;
+        this.cabeca.rotation.x += (this.headRX - this.cabeca.rotation.x) / velocidade;
+        
+    }
 }
 
+function calc(v, vmin, vmax, tmin, tmax) {
+    var nv = Math.max(Math.min(v, vmax), vmin)
+    var dv = vmax - vmin
+    var pc = (nv - vmin) / dv 
+    var dt = tmax - tmin 
+    var tv = tmin + (pc * dt)
+
+    return tv
+}
 function loop() {
     renderizador.render(cena, camera)
-    // walter.update()
+    walter.atualizar()
     requestAnimationFrame(loop)
 }
 init()
