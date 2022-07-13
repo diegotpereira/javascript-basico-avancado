e3d7727e0c2994e91cbf1ab02d1b5707722a133c
diegotpereira <diegoteixeirapereira@hotmail.com>
2022-07-13 11:04:49 -0300

    criação do app com exemplo numero.

---
+++ tipo-number/js/app.js
@@ -0,0 +1,23 @@
+var numeroObjeto = new Number(100)
+
+console.log(numeroObjeto.valueOf());
+
+var aNumero = new Number(10)
+
+console.log(aNumero.toString());
+console.log(aNumero.toString(2));
+
+let x = 10
+
+console.log(x.toString(16));
+
+var distancia = 19.006
+console.log(distancia.toFixed(2));
+
+distancia = 19.004
+console.log(distancia.toFixed(2));
+
+var x1 = 10, y = 100, z = 1000
+console.log(x1.toExponential());
+console.log(y.toExponential());
+console.log(z.toExponential());
