853cde8893ff41878dd1fbe56993ce0851de5879
diegotpereira <diegoteixeirapereira@hotmail.com>
2022-08-03 13:59:40 -0300

    criação do metodo iniciar terminar posição.

--- exemplos-praticos/jogo-labirinto-picles/js/app.js
+++ exemplos-praticos/jogo-labirinto-picles/js/app.js
@@ -1,6 +1,7 @@
 var labirintoTela = document.getElementById('labirintoTela')
 var ctx = labirintoTela.getContext('2d')
 var duende
+var finalDuende
 var labirinto, desenhar, jogador
 var dificuldade
 
@@ -20,6 +21,45 @@ window.onload = function() {
     duende.src = "https://image.ibb.co/dr1HZy/Pf_RWr3_X_Imgur.png" + 
     "?" +
     new Date().getTime()
+    duende.setAttribute("crossOrigin", " ")
+    duende.onload = function() {
+        duende = changeBrightness(1.2, duende)
+
+    }
+    finalDuende = new Image()
+    finalDuende.src = "https://image.ibb.co/b9wqnJ/i_Q7m_U25_Imgur.png" +
+    "?" + 
+    new Date().getTime()
+    finalDuende.setAttribute("crossOrigin", " ")
+    finalDuende.onload = function() {
+        finalDuende = changeBrightness(1.1, finalDuende)
+    }
+}
+
+function changeBrightness(fator, duende) {
+    
+    var virtCanvas = document.createElement("canvas")
+    virtCanvas.width = 500
+    virtCanvas.height = 500
+
+    var context = virtCanvas.getContext('2d')
+    context.drawImage(duende, 0, 0, 500, 500)
+    
+    var imgDado = context.getImageData(0, 0, 500, 500)
+
+    for(let i = 0; i < imgDado.data.length; i += 4) {
+        imgDado.data[i] = imgDado.data[i] * fator
+        imgDado.data[i + 1] = imgDado.data[i + 1] * fator
+        imgDado.data[i + 2] = imgDado.data[i + 2] * fator
+    }
+    context.putImageData(imgDado, 0, 0)
+
+    var duendeSaidaPut = new Image()
+    duendeSaidaPut.src = virtCanvas.toDataURL()
+    virtCanvas.remove()
+
+    return duendeSaidaPut
+
 }
 
 window.onresize = function() {
@@ -36,11 +76,11 @@ window.onresize = function() {
     }
     cellSize = labirintoTela.width / dificuldade
 }
-function Labirinto(Largura, Altura) {
+function Labirinto(Width, Height) {
 
     var labirintoMapa 
-    var largura = Largura
-    var altura = Altura
+    var width = Width
+    var height = Height
     var iniciarCoord, fimCoord
     var dirs = ["n", "s", "e", "w"]
     var modDir = {
@@ -75,12 +115,12 @@ function Labirinto(Largura, Altura) {
         return fimCoord
     }
     function genMap() {
-        labirintoMapa = new Array(altura)
+        labirintoMapa = new Array(height)
 
-        for(y = 0; y < altura; y++) {
-            labirintoMapa[y] = new Array(largura)
+        for(y = 0; y < height; y++) {
+            labirintoMapa[y] = new Array(width)
 
-            for(x = 0; x < largura; ++x) {
+            for(x = 0; x < width; ++x) {
                 labirintoMapa[y][x] = {
                     n: false,
                     s: false,
@@ -92,10 +132,126 @@ function Labirinto(Largura, Altura) {
             }
         }
     }
+    function definirLabirinto() {
+        var ehComp = false
+        var mover = false
+        var cellsVisited = 1
+        var numLoops = 0
+        var maxLoops = 0
+        var pos = {
+            x: 0,
+            y: 0
+        }
+        var numCells = width * height
+
+        while(!ehComp) {
+            mover = false
+            labirintoMapa[pos.x][pos.y].visitado = true 
+
+            if (numLoops >= maxLoops) {
+                shuffle(dirs)
+                maxLoops = Math.round(rand(height / 8))
+                numLoops = 0
+            }
+            numLoops++
+
+            for(index = 0; index < dirs.length; index++) {
+                var direcao = dirs[index]
+                var nx = pos.x + modDir[direcao].x
+                var ny = pos.y + modDir[direcao].y
+
+                if (nx >= 0 && nx < width && ny >= 0 && ny < height) {
+                    
+                    if (!labirintoMapa[nx][ny].visitado) {
+                        
+                        labirintoMapa[pos.x][pos.y][direcao] = true 
+                        labirintoMapa[nx][ny][modDir[direcao].o] = true
+
+                        labirintoMapa[nx][ny].priorPos = pos 
+
+                        pos = {
+                            x: nx,
+                            y: ny 
+                        }
+                        cellsVisited++ 
+
+                        mover = true 
+                        break
+                    }
+                }
+            }
+            if (!mover) {
+                
+                pos = labirintoMapa[pos.x][pos.y].priorPos
+            }
+            if (numCells == cellsVisited) {
+                ehComp = true 
+            }
+        }
+    }
+    function definirIniciarTerminar() {
+        switch(rand(4)) {
+            case 0:
+                iniciarCoord = {
+                    x: 0,
+                    y: 0
+                }
+                fimCoord= {
+                    x: height - 1,
+                    y: width - 1
+                }
+                break
+            case 1: 
+            iniciarCoord= {
+                x: 0,
+                y: width - 1
+            }
+            fimCoord = {
+                x: height - 1,
+                y: 0
+            }
+            break
+
+            case 2:
+                iniciarCoord = {
+                  x: height - 1,
+                  y: 0
+                };
+                fimCoord = {
+                  x: 0,
+                  y: width - 1
+                };
+                break;
+            case 3:
+                iniciarCoord = {
+                    x: height - 1,
+                    y: width - 1
+                };
+                fimCoord = {
+                    x: 0,
+                    y: 0
+                };
+                break;
+        }
+    }
     genMap()
+    definirLabirinto()
+    definirIniciarTerminar()
 }
 
-function DesenharLabirinto(Labirinto, ctx, cellSize) {
+function rand(max) {
+    return Math.floor(Math.random() * max)
+}
+function shuffle(a) {
+
+    for(let i = a.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1))
+        [a[i], a[j]] = [a[j], a[i]]
+    }
+    return a
+}
+
+function desenharLabirinto(Labirinto, ctx, cellSize) {
     var mapa = Labirinto.map()
     var cellSize = cellSize
     var desenharFinalMetodo
@@ -119,19 +275,19 @@ function DesenharLabirinto(Labirinto, ctx, cellSize) {
             ctx.lineTo(x + cellSize, y)
             ctx.stroke()
         }
-        if (cell.s == false) {
+        if (cell.s === false) {
             ctx.beginPath()
             ctx.moveTo(x, y + cellSize)
             ctx.lineTo(x + cellSize, y + cellSize)
             ctx.stroke()
         }
-        if (cell.e == false) {
+        if (cell.e === false) {
             ctx.beginPath()
             ctx.moveTo(x + cellSize, y)
             ctx.lineTo(x + cellSize, y + cellSize)
             ctx.stroke()
         }
-        if (cell.w == false) {
+        if (cell.w === false) {
             ctx.beginPath()
             ctx.moveTo(x,  y)
             ctx.lineTo(x, y + cellSize)
@@ -141,8 +297,8 @@ function DesenharLabirinto(Labirinto, ctx, cellSize) {
 
     function desenharMapa() {
 
-        for(x = 0; x < mapa[x].length; x++) {
-            for(y = 0; y < mapa[y].length; y++) {
+        for(x = 0; x < mapa.length; x++) {
+            for(y = 0; y < mapa[x].length; y++) {
                 desenharCelula(x, y, mapa[x][y])
             }
         }
@@ -154,7 +310,7 @@ function fazerLabirinto() {
 
     cellSize = labirintoTela.width / dificuldade
     labirinto = new Labirinto(dificuldade, dificuldade)
-    desenhar = new DesenharLabirinto(labirinto, ctx, cellSize)
+    desenhar = new desenharLabirinto(labirinto, ctx, cellSize)
 
     if (document.getElementById('labirintoContainer').style.opacity < "100") {
         document.getElementById('labirintoContainer').style.opacity = "100"
