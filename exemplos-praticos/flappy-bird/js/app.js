6447f80fa5c0259ccd9396c28aaf612b5b9880b4
diegotpereira <diegoteixeirapereira@hotmail.com>
2022-08-07 13:53:17 -0300

    alteração no javascript. adicionado metodos.

--- exemplos-praticos/flappy-bird/js/app.js
+++ exemplos-praticos/flappy-bird/js/app.js
@@ -2,11 +2,13 @@ document.addEventListener('DOMContentLoaded', () => {
 
     const passaro = document.querySelector('.passaro')
     const jogoExibir = document.querySelector('.jogo-container')
-    const chao = document.querySelector('.chao')
+    const chao = document.querySelector('.chao-movendo')
     
     let passaroEsquerdo = 220
     let passaroFundo = 100
     let gravidade = 3
+    let ehFimDeJogo = false
+    let gap = 430
 
     function iniciarJogo() {
         passaroFundo -= gravidade
@@ -14,10 +16,10 @@ document.addEventListener('DOMContentLoaded', () => {
         passaro.style.left = passaroEsquerdo + 'px'
     }
 
-    let tempoId = setInterval(iniciarJogo, 20)
+    let jogotempoId = setInterval(iniciarJogo, 20)
 
     // clearInterval(tempoId)
-    function control(e) {
+    function controle(e) {
         if (e.keyCode === 32) {
             pular()
         }
@@ -29,5 +31,67 @@ document.addEventListener('DOMContentLoaded', () => {
         passaro.style.bottom = passaroFundo + 'px'
         console.log(passaroFundo);
     }
-    document.addEventListener('keyup', pular)
+    document.addEventListener('keyup', controle)
+
+    function gerarObstaculo() {
+        
+        let obstaculoRestante = 500
+        let aleatoriaAltura = Math.random() * 60
+        let obstaculoBottom = aleatoriaAltura
+
+        const obstaculo = document.createElement('div')
+        const topoObstaculo = document.createElement('div')
+
+        if (!ehFimDeJogo) {
+            obstaculo.classList.add('obstaculo')
+            topoObstaculo.classList.add('topoObstaculo')
+        }
+        jogoExibir.appendChild(obstaculo)
+        jogoExibir.appendChild(topoObstaculo)
+
+        jogoExibir.appendChild(obstaculo)
+        jogoExibir.appendChild(topoObstaculo)
+
+        obstaculo.style.left = obstaculoRestante + 'px'
+        topoObstaculo.style.left = obstaculoRestante +  'px'
+
+        obstaculo.style.bottom = obstaculoBottom + 'px'
+        topoObstaculo.style.bottom = obstaculoBottom + gap + 'px'
+
+        function moverObstaculo() {
+
+            obstaculoRestante -=2
+
+            obstaculo.style.left = obstaculoRestante + 'px'
+            topoObstaculo.style.left = obstaculoRestante + 'px'
+
+            if (obstaculoRestante === -60) {
+                
+                clearInterval(tempoId)
+
+                jogoExibir.removeChild(obstaculo)
+                jogoExibir.removeChild(topoObstaculo)
+            }
+            if (obstaculoRestante > 200 && obstaculoRestante < 280 && passaroEsquerdo === 220 &&
+                (passaroFundo < obstaculoBottom + 153 || passaroFundo > obstaculoBottom + gap - 200) ||
+                passaroFundo === 0) {
+                
+                    clearInterval(tempoId)
+            }
+        }
+        let tempoId = setInterval(moverObstaculo, 20)
+        if(!ehFimDeJogo) setInterval(gerarObstaculo, 3000)
+    }
+    gerarObstaculo()
+
+    function fimDeJogo() {
+        clearInterval(jogotempoId)
+        console.log('Fim de Jogo');
+        ehFimDeJogo = true
+
+        document.removeEventListener('keyup', controle)
+
+        chao.classList.add('chao')
+        chao.classList.remove('chao-movendo')
+    }
 })
