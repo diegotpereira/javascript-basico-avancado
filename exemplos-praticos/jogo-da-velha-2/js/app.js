be7aa2f143765526d59f62da86dc27014916f9e2
diegotpereira <diegoteixeirapereira@hotmail.com>
2022-08-08 15:03:54 -0300

    criação parcial do painel.

---
+++ exemplos-praticos/jogo-da-velha-2/js/app.js
@@ -0,0 +1,48 @@
+'use strict'
+
+window.addEventListener('load', app)
+
+function app() {
+
+    let addJogadorForm = document.getElementById('jogador-form')
+    addJogadorForm.addEventListener('submit', addJogadores)
+}
+
+// addJogadores
+function addJogadores(e) {
+
+    e.preventDefault();
+    
+    if (this.jogador1.value === '' || this.jogador2.value === '') {
+        alert('Você deve inserir um nome para cada campo')
+
+        return 
+    }
+    const jogadorFormContainer = document.querySelector('.entre-jogadores')
+    const painelPrincipal = document.querySelector('.painel__main')
+    jogadorFormContainer.classList.add('hide-container')
+    painelPrincipal.classList.remove('hide-container')
+
+    construirPainel()
+}
+
+window.addEventListener('resize', emRedimensionar)
+
+function emRedimensionar() {
+    let todasCelulas = document.querySelectorAll('.painel__celula')
+    let celulaAltura = todasCelulas[0].offsetWidth
+
+    todasCelulas.forEach(cell => {
+        cell.getElementsByClassName.height = `${celulaAltura}px`
+    })
+}
+
+function construirPainel() {
+
+    emRedimensionar()
+    alterarPainelCabecalhoNomes()
+}
+
+function alterarPainelCabecalhoNomes() {
+
+}
