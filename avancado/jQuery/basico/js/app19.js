08fd4a8952fdf9e804413779d132ae325d746fb7
diegotpereira <diegoteixeirapereira@hotmail.com>
2022-11-20 21:40:23 -0300

    alteração js

--- avancado/jQuery/basico/js/app19.js
+++ avancado/jQuery/basico/js/app19.js
@@ -4,8 +4,27 @@
 //     });
 // });
 
-$(document).ready(function() {
-    $("#btn1").click(function() {
-        $("p").prepend("<b>Texto anexado</b>.");
-    });
-});
+// 1) Programa exemplo Prepend()
+
+// $(document).ready(function() {
+//     $("#btn1").click(function() {
+//         $("p").prepend("<b>Anexar texto</b>.");
+//     });
+// });
+
+
+// // 2) Programa exemplo Prepend()
+
+// $(document).ready(function() {
+//     $("#btn1").click(function() {
+//         $("p").prepend("<b>Anexar texto</b>");
+//     });
+//     $("#btn2").click(function() {
+//         $("ol").prepend("<li>Anexar item</li>");
+//     });
+// });
+
+
+// 3) Programa exemplo Prepend()
+
+$("p").prepend("<b> Ola </<b>");
---
+++ avancado/jQuery/basico/js/app23.js
@@ -0,0 +1,21 @@
+// O método jQuery empty() é usado para remover todos os nós 
+// filhos e o conteúdo dos elementos selecionados. Este método 
+// não remove o próprio elemento.
+
+// // exemplo 1
+
+// $(document).ready(function() {
+//     $("button"),click(function() {
+//         $("div").empty();
+//     });
+// });
+
+
+// // exemplo 2
+
+// $(document).ready(function() {
+//     $("div").click(function() {
+//         $(this).empty();
+//     });
+// });
+
